94afd062d11991fdf04769e175c456f1742361ea
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2015-11-04 00:03:50 -0800

    solution (basic requirements)

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 // wait for DOM to load before running JS
-$(function() {
+$(document).ready(function() {
 
-  // check to make sure JS is loaded
   console.log('JS is loaded!');
-  
-  // your code here
 
+  $("#search").on("submit", function(event){
+      $("li").remove(); // intiate the page every new search 
+  	     event.preventDefault();
+  	     var link = $("#track").val();
+  	       $.get("https://api.spotify.com/v1/search?q=" + link + "&type=track", function (data){ 
+              console.log(data);
+  		        var result = data.tracks.items;
+  		        result.forEach(function(item){
+  			       var artist = item.artists[0].name;
+  			       var track = item.name;
+               //var artwork = item.album.images[0].url;
+               //console.log (artwork); 
+  			       var appended = "<li>Artist: " + artist + " track: " + track + "</li>";
+  			       console.log(appended);
+  			       $("#artistTrack").append(appended);
+  			     });
+  	     });
+  
+ });
 });
